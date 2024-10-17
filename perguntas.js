996c4e0336d5865b6295f441b1738786fe104a99
Kauan Veiga Sabino <149194613+kauveiga2008@users.noreply.github.com>
2024-10-17 16:39:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Filme',
+    'Em que filme o personagem Michael Corleone é interpretado por Al Pacino?',
+    'O Poderoso Chefão'
+)
+
+criaCartao(
+    'Filme',
+    'Em qual filme da Marvel Thor enfrenta Hela, sua irmã?',
+    'Thor: Ragnarok'
+)
+
+criaCartao(
+    'Filme',
+    'Qual o nome da princesa que protagoniza o filme A Bela Adormecida?',
+    'Aurora'
+)
+
+criaCartao(
+    'Filme',
+    'Qual o nome do brinquedo astronauta em Toy Story?',
+    'Buzz Lightyear'
+)
+
+criaCartao(
+    'Filme',
+    'Qual é o nome do dragão que acompanha a personagem Mulan?',
+    'Mushu'
+)
+
+criaCartao(
+    'Filme',
+    'Em qual filme o personagem Woody é um cowboy?',
+    'Toy Story'
+)
+
+criaCartao(
+    'Filme',
+    'Qual ator protagoniza o filme Missão impossível?',
+    'Tom Cruise'
+)
+
+criaCartao(
+    'Filme',
+    'Qual o nome do androide em "O Exterminador do Futuro" interpretado por Arnold Schwarzenegger?',
+    'T-800'
+)
+
+criaCartao(
+    'Filme',
+    'Qual foi o primeiro longa-metragem lançado pela Disney?',
+    'Branca de Neve e os Sete Homens com Nanismo'
+)
